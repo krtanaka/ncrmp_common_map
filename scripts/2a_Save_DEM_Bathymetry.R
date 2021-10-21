3fca9282a04ce29970a3f8f45639cc675ebec4a1
Kisei Tanaka <kisei.tanaka@gmail.com>
2021-10-20 17:05:16 -1000

    ok

--- scripts/2a_Save_DEM_Bathymetry.R
+++ scripts/2a_Save_DEM_Bathymetry.R
@@ -4,19 +4,22 @@
 #############################################################################
 topo = raster("L:/ktanaka/GIS/bathymetry/mariana_trench_6_msl_2012.nc")
 topo = as.data.frame(rasterToPoints(topo))
-topo$Topography = ifelse(topo$Topography %in% c(-30:0), topo$Topography, NA)
+topo$Topography = ifelse(topo$GDAL.Band.Number.1 %in% c(-30:0), topo$GDAL.Band.Number.1, NA)
 topo = topo %>% drop_na()
 
+# Auijan
+topo = topo %>% 
+  subset(x > 145.5 & x < 145.6) %>% 
+  subset(y > 14.8 & y < 14.9)
+
 topo %>%
-  group_by(x, y) %>%
-  summarise(x = mean(x),
-            y = mean(y),
-            Topography = mean(Topography, na.rm = T)) %>%
-  ggplot(aes(x, y, fill = Topography)) +
-  geom_tile(aes(width = 0.005, height = 0.005)) +
+  ggplot(aes(x, y, fill = GDAL.Band.Number.1)) +
+  # geom_tile(aes(width = 0.005, height = 0.005)) +
+  geom_raster() + 
   scale_fill_viridis_c() +
   coord_fixed() +
   ggdark::dark_theme_minimal() +
   theme(axis.title = element_blank())
 
 save(topo, file = 'data/Topography_NOAA_CRM_vol10.RData')
+save(topo, file = 'data/gis_bathymetry/raster/agu.RData')
