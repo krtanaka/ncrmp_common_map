f6d18f572942de4c524df4ebce70b07ab504b97d
Kisei Tanaka <kisei.tanaka@gmail.com>
2022-01-11 11:16:30 -1000

    ok

--- scripts/4_Make_Survey_Domain_v2.R
+++ scripts/4_Make_Survey_Domain_v2.R
@@ -26,7 +26,7 @@ islands = c("agr", "ala", "asc", "gug", "fdp", "mau", "sar"); region = "N.MARIAN
 
 for (isl in 1:length(islands)) {
   
-  # isl = 1
+  # isl = 3
   
   load(paste0("data/gis_bathymetry/", islands[isl], ".RData"))
   
@@ -50,6 +50,8 @@ for (isl in 1:length(islands)) {
     load(paste0("data/gis_bathymetry/", islands[isl], ".RData"))
     sector = topo_i
     sector[sector <= 0] <- 1
+    sector_name = data.frame(ID = 1L, 
+                             nam = paste0(islands[isl], "_sector"))
     
   }
   
@@ -68,6 +70,8 @@ for (isl in 1:length(islands)) {
     load(paste0("data/gis_bathymetry/", islands[isl], ".RData"))
     reef = topo_i
     reef[reef <= 0] <- 1
+    reef_name = data.frame(ID = 1L, 
+                           nam = "forereef")
     
   }
   
@@ -86,118 +90,135 @@ for (isl in 1:length(islands)) {
     load(paste0("data/gis_bathymetry/", islands[isl], ".RData"))
     hardsoft = topo_i
     hardsoft[hardsoft <= 0] <- 1
+    hardsoft_name = data.frame(ID = 1L, 
+                               nam = paste0(islands[isl], "_hardsoft"))
     
   }
   
-  default_proj = "+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
+  hardsoft = resample(hardsoft, topo_i, method = "ngb") 
+  sector = resample(sector, topo_i, method = "ngb") 
+  reef = resample(reef, topo_i, method = "ngb") 
+  bathymetry = resample(topo_i, topo_i, method = "ngb") 
   
-  crs(topo_i) = default_proj
-  crs(sector) = default_proj
-  crs(reef) = default_proj
-  crs(hardsoft) = default_proj
+  df = stack(hardsoft, sector, reef, bathymetry)
+  df = as.data.frame(rasterToPoints(df))
   
-  names(topo_i) = "depth"
-  names(sector) = "sector"
-  names(reef) = "reef"
-  names(hardsoft) = "hardsoft"
+  colnames(df) = c("longitude", "latitude", "hardsoft", "sector", "reef", "depth")
   
-  sector = resample(sector, topo_i, method = "bilinear"); plot(sector)
-  reef = resample(reef, topo_i, method = "bilinear"); plot(reef)
-  hardsoft = resample(hardsoft, topo_i, method = "bilinear"); plot(hardsoft)
+  df = na.omit(df)
   
-  ncrmp = stack(sector, topo_i)
-  ncrmp = stack(reef, ncrmp)
-  ncrmp = stack(hardsoft, ncrmp)
+  df$cell = 1:dim(df)[1]; df$cell = as.numeric(df$cell)
+  df$division = as.numeric(1)
   
-  plot(ncrmp)
+  df$depth_bin = ""
+  df$depth_bin = ifelse(df$depth <= 0  & df$depth >= -6, 1L, df$depth_bin) 
+  df$depth_bin = ifelse(df$depth < -6  & df$depth >= -18, 2L, df$depth_bin) 
+  df$depth_bin = ifelse(df$depth < -18, 3L, df$depth_bin) 
   
-  ncrmp = rasterToPoints(ncrmp) %>% as.data.frame() %>% na.omit(); head(ncrmp)
+  df$hardsoft = round(df$hardsoft, 0)
+  df$reef = round(df$reef, 0)
+  df$sector = round(df$sector, 0)
   
-  ncrmp$cell = 1:dim(ncrmp)[1]; ncrmp$cell = as.numeric(ncrmp$cell)
+  df$sector = as.character(df$sector)
+  df$reef = as.character(df$reef)
   
-  ncrmp$depth_bin = ""
-  ncrmp$depth_bin = ifelse(ncrmp$depth <= 0  & ncrmp$depth >= -6, 1L, ncrmp$depth_bin) 
-  ncrmp$depth_bin = ifelse(ncrmp$depth < -6  & ncrmp$depth >= -18, 2L, ncrmp$depth_bin) 
-  ncrmp$depth_bin = ifelse(ncrmp$depth < -18, 3L, ncrmp$depth_bin) 
+  df$depth = as.numeric(df$depth*-1)
   
-  ncrmp$sector = as.character(round(ncrmp$sector, 0))
-  ncrmp$reef = as.character(round(ncrmp$reef, 0))
+  df$longitude = df$longitude * 0.001
+  df$latitude = df$latitude * 0.001
   
-  ncrmp$strat = paste(ncrmp$depth_bin, 
-                      ncrmp$sector,
-                      ncrmp$reef,
-                      sep = "_")
+  df = as.data.frame(df)
   
-  ncrmp$strat = as.numeric(as.factor(ncrmp$strat))
+  colnames(sector_name) = c("sector", "sector_id")
+  colnames(reef_name) = c("reef", "reef_id")
+  colnames(hardsoft_name) = c("hardsoft", "hardsoft_id")
   
-  ncrmp$depth = as.numeric(ncrmp$depth*-1)
+  sector_name$sector_id = tolower(sector_name$sector_id)
+  reef_name$reef_id = tolower(reef_name$reef_id)
+  hardsoft_name$hardsoft_id = tolower(hardsoft_name$hardsoft_id)
   
-  colnames(ncrmp)[1:2] = c("longitude", "latitude")
+  df = merge(df, sector_name)
+  df = merge(df, reef_name)
+  df = merge(df, hardsoft_name)
   
-  ncrmp$longitude = ncrmp$longitude * 0.001
-  ncrmp$latitude = ncrmp$latitude * 0.001
-  
-  (depth = ncrmp %>% 
+  (depth = df %>% 
       ggplot( aes(longitude, latitude, fill = depth_bin)) + 
       geom_raster() + 
       scale_fill_discrete( "depth_bins") +
-      coord_fixed() +
-      ggdark::dark_theme_minimal() +
-      theme(axis.title = element_blank(),
-            legend.position = "right"))
+      ggdark::dark_theme_minimal())
   
-  (sector = ncrmp %>% 
-      ggplot( aes(longitude, latitude, fill = factor(sector))) + 
+  (sector = df %>% 
+      ggplot( aes(longitude, latitude, fill = sector_id)) + 
       geom_raster() +
       scale_fill_discrete("sector") +
-      coord_fixed() +
-      ggdark::dark_theme_minimal() +
-      theme(axis.title = element_blank(),
-            legend.position = "right"))
+      ggdark::dark_theme_minimal())
   
-  (reef = ncrmp %>% 
-      ggplot( aes(longitude, latitude, fill = as.factor(reef))) + 
+  (reef = df %>% 
+      ggplot( aes(longitude, latitude, fill = reef_id)) + 
       geom_raster() +
       scale_fill_discrete("reef") +
-      coord_fixed() +
-      theme_minimal() + 
-      ggdark::dark_theme_minimal() +
-      theme(axis.title = element_blank(),
-            legend.position = "right"))
+      ggdark::dark_theme_minimal())
   
-  (strata = ncrmp %>% 
+  (hardsoft = df %>% 
+      ggplot( aes(longitude, latitude, fill = hardsoft_id)) + 
+      geom_raster() +
+      scale_fill_discrete("hardsoft") +
+      ggdark::dark_theme_minimal())
+  
+  df = df %>%
+    # subset(sector_id != "GUA_LAND") %>% # filter sector
+    # subset(reef_id %in% c( "Forereef")) %>% # filter land and Reef Crest/Reef Flat
+    subset(hardsoft_id %in% c("hard", "unknown")) # filter for sector
+  
+  df$strat = paste(df$depth_bin, 
+                   df$sector,
+                   df$reef,
+                   sep = "_")
+  
+  df$strat = as.numeric(as.factor(df$strat))
+  
+  ################################################################
+  ### create table to match strata to numbers for output table ###
+  ################################################################
+  tab <- df 
+  tab$depth_bin_value = ""
+  tab$depth_bin_value = ifelse(tab$depth_bin == 1, "SHAL", tab$depth_bin_value) 
+  tab$depth_bin_value = ifelse(tab$depth_bin == 2, "MIDD", tab$depth_bin_value) 
+  tab$depth_bin_value = ifelse(tab$depth_bin == 3, "DEEP", tab$depth_bin_value) 
+  
+  tab <- tab %>% dplyr::select(sector_id,reef_id,strat,depth_bin_value)
+  tab <- tab %>% filter(!duplicated(tab))
+  
+  save(tab,file = paste0("outputs/sector_key_", islands[isl], ".RData"))
+  
+  (strata = df %>% 
       ggplot( aes(longitude, latitude, fill = factor(strat))) + 
       geom_raster() +
       scale_fill_discrete("Strata") +
-      coord_fixed() +
-      ggdark::dark_theme_minimal() +
-      theme(axis.title = element_blank(),
-            legend.position = "right"))
+      ggdark::dark_theme_minimal())
   
-  ncrmp = as.data.frame(ncrmp)
-  
-  cell = rasterFromXYZ(ncrmp[,c("longitude", "latitude", "cell")]); plot(cell)
-  strat = rasterFromXYZ(ncrmp[,c("longitude", "latitude", "strat")]); plot(strat)
-  depth = rasterFromXYZ(ncrmp[,c("longitude", "latitude", "depth")]); plot(depth)
+  cell = rasterFromXYZ(df[,c("longitude", "latitude", "cell")]); plot(cell)
+  division = rasterFromXYZ(df[,c("longitude", "latitude", "division")]); plot(division)
+  strat = rasterFromXYZ(df[,c("longitude", "latitude", "strat")]); plot(strat)
+  depth = rasterFromXYZ(df[,c("longitude", "latitude", "depth")]); plot(depth)
   
   values = raster::values
   
-  survey_grid_ncrmp = stack(cell, strat, depth)
+  survey_grid_ncrmp = stack(cell, division, strat, depth)
   survey_grid_ncrmp$strat = round(survey_grid_ncrmp$strat, digits = 0)
+  values(survey_grid_ncrmp$division) = ifelse(is.na(values(survey_grid_ncrmp$division)), NA, 1)
   
-  sp::spplot(survey_grid$strat) #SimSurvey example
-  sp::spplot(survey_grid_ncrmp$strat)
+  survey_grid_ncrmp = readAll(survey_grid_ncrmp)
   
-  sp::spplot(survey_grid$depth) #SimSurvey example
-  sp::spplot(survey_grid_ncrmp$depth)
-  
+  # default_proj = "+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
+  # crs(survey_grid_ncrmp) = default_proj
+
   p <- raster::rasterToPolygons(survey_grid$strat, dissolve = TRUE); sp::plot(p)
   p <- raster::rasterToPolygons(survey_grid_ncrmp$strat, dissolve = TRUE); sp::plot(p)
   
   survey_grid_ncrmp = readAll(survey_grid_ncrmp)
-  
   save(survey_grid_ncrmp, file = paste0("data/survey_grid_ncrmp/survey_grid_", islands[isl], ".RData"))
   
-  print(paste0("...", islands[isl], " survey domain generated..."))
+  print(paste0("... ", islands[isl], " survey domain generated ..."))
   
 }
