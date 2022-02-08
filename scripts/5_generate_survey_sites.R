1d9c731dfed42c652caf50171be94bb4bd225029
krtanaka <kisei.tanaka@gmail.com>
2022-02-07 16:20:44 -1000

    Update 5_generate_survey_sites.R

--- scripts/5_generate_survey_sites.R
+++ scripts/5_generate_survey_sites.R
@@ -246,10 +246,11 @@ for (i in 1:length(islands)) {
       # geom_tile(data = boxes, aes(longitude, latitude, fill = nam), width = 0.001, height = 0.001, alpha = 0.2, show.legend = F) +
       
       geom_polygon(data = hulls, aes(longitude, latitude, fill = nam, color = nam), alpha = 0.01, size = 2, show.legend = F) +
-    
+
       # geom_path(data = ISL_this, aes(long, lat, group = group), inherit.aes = F, size = 0.05, color = "darkgrey") +
       # geom_polygon(data = ISL_this, aes(long, lat, group = group), fill = "darkgrey", color = NA, alpha = 0.9) +
       
+      new_scale_color() +
       geom_point(data = sets, aes(longitude, latitude, shape = depth_bin, color = depth_bin)) +
       
       geom_text_repel(data = labels, aes(longitude, latitude, label = nam), max.overlaps = Inf) +
