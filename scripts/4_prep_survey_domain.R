9dc6d3a6875b1f46abcb3a1fcb9eec3d7b20fab9
Kisei Tanaka <kisei.tanaka@gmail.com>
2022-06-06 15:09:05 -1000

    ok

--- scripts/4_prep_survey_domain.R
+++ scripts/4_prep_survey_domain.R
@@ -19,16 +19,18 @@ utm = read_csv('data/misc/ncrmp_utm_zones.csv')
 
 #islands = c("gua", "rot", "sai", "tin", "agu"); region = "S.MARIAN"                           # South Mariana Islands
 #islands = c("agr", "ala", "asc", "gug", "fdp", "mau", "pag", "sar"); region = "N.MARIAN"      # North Mariana Islands
-islands = c("ofu", "ros", "swa", "tau", "tut"); region = "SAMOA"                              # American Samoa
+# islands = c("ofu", "ros", "swa", "tau", "tut"); region = "SAMOA"                              # American Samoa
+
 islands = c("bak", "how", 
-            # "jar", 
+            # "jar", # shapeifle projections do not match with bathymetry projections
             "joh", "kin", "pal", "wak"); region = "PRIAs"                # Pacific Remote Island Areas
+
 # islands = c("haw", "kah", "kal", "kau", "lan", "mai", "mol", "nii", "oah"); region = "MHI"  # Main Hawaiian Islands
 # islands = c("ffs", "kur", "lay", "lis", "mar", "mid", "phr"); region = "NWHI"               # Northern Hawaiian Islands
 
 for (isl in 1:length(islands)) {
   
-  isl = 6
+  # isl = 6
   
   load(paste0("data/gis_bathymetry/", islands[isl], ".RData"))
   
