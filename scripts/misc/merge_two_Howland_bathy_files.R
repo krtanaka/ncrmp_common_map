a703527a131fd88499a6fb3351780e58f1bcb132
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-01-13 10:04:43 -1000

    Update merge_two_Howland_bathy_files.R

--- scripts/misc/merge_two_Howland_bathy_files.R
+++ scripts/misc/merge_two_Howland_bathy_files.R
@@ -1,13 +1,15 @@
 # merge two bathymetry files for Howland to keep enough shallow sites
 
+library(raster)
+
 # Howland Island: Bathymetry
 # https://www.soest.hawaii.edu/pibhmc/cms/data-by-location/pacific-remote-island-area/howland-island/howland-island-bathymetry/
 
 # NOAA 2m derived depth from WorldView-2 multispectral satellite imagery. WV-2 derived depths bathy data
-how1 = raster("N:/GIS/Projects/CommonMaps/Bathymetry/how_dball.asc")
+how1 = raster("N:/GIS/Projects/CommonMaps/Bathymetry/how_dball.asc"); plot(how1)
 
 # CRED 5 m Gridded bathymetry of Howland Island, Pacific Remote Island Areas, Central Pacific (Arc ASCII Format)
-how2 = raster("N:/GIS/Projects/CommonMaps/Bathymetry/Howland_5m.asc")
+how2 = raster("N:/GIS/Projects/CommonMaps/Bathymetry/Howland_5m.asc"); plot(how2)
 
 how1 = resample(how1, how2, method = "ngb") 
 
