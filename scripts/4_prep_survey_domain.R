52cbce9203e370efa64d06239c2e7cbbed8ddc5d
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-01-27 13:24:10 -1000

    ok

--- scripts/4_prep_survey_domain.R
+++ scripts/4_prep_survey_domain.R
@@ -21,7 +21,7 @@ islands = c("gua", "rot", "sai", "tin", "agu"); region = "S.MARIAN"             
 islands = c("agr", "ala", "asc", "gug", "fdp", "mau", "pag", "sar"); region = "N.MARIAN"      # North Mariana Islands
 islands = c("ofu", "ros", "swa", "tau", "tut"); region = "SAMOA"                              # American Samoa
 islands = c("bak", "how", "jar", "joh", "kin", "pal", "wak"); region = "PRIAs"                # Pacific Remote Island Areas
-# islands = c("haw", "kah", "kal", "kau", "lan", "mai", "mol", "nii", "oah"); region = "MHI"  # Main Hawaiian Islands
+islands = c("haw", "kah", "kal", "kau", "lan", "mai", "mol", "nii", "oah"); region = "MHI"  # Main Hawaiian Islands
 # islands = c("ffs", "kur", "lay", "lis", "mar", "mid", "phr"); region = "NWHI"               # Northern Hawaiian Islands
 
 for (isl in 1:length(islands)) {
