1beb971960bb0a5c13a3814de4911d4c6a8b8dd7
thomas-oliver <thomas.oliver@gmail.com>
2023-11-15 09:41:20 -1000

    utm_zones EPSG code addition

--- scripts/6_Convert_SurveyGrids_To_Strata_Polygons.R
+++ scripts/6_Convert_SurveyGrids_To_Strata_Polygons.R
@@ -1,15 +1,19 @@
+rm(list=ls())
 library(raster)
 library(terra)
 library(sf)
 library(stars)
 
 grids=list.files("./data/survey_grid_ncrmp/",full.names = T)
+drop_grids=which(regexpr(pattern = "Oahu",text = grids)>0)
+grids=grids[-drop_grids]
+
 keys=list.files("./outputs/sector_key/",full.names = T)
 
 
-#for(i in 1:length(grids)){
-for(i in c(7,9,22)){
-    #i=7
+for(i in 1:length(grids)){
+#for(i in c(7,9,22)){
+  #i=1
   #Get the right grid and sector key
   grid_file=grids[i]
   grid_isl=substr(grid_file,38,40)
@@ -18,15 +22,19 @@ for(i in c(7,9,22)){
   #load raster and convert into SpatRaster
   load(grid_file) #called survey_grid_ncrmp
   survey_grid_ncrmp.sr=as(survey_grid_ncrmp,"SpatRaster")
-  
-  #generate str_name in lookup table
+  #load keys and convert into str_name
   load(keys_file)
+  #generate str_name in lookup table
   tab$strata_id=paste0(tab$sector_id,"_",substr(tab$depth_bin_value,1,1))
-  tabLU=tab$strata_id;names(tabLU)=tab$strat
+  tabLU=tab$strata_id
+  names(tabLU)=tab$strat
+  
+  
   #convert SpatRaster to polygon data
   survey_grid_ncrmp.sh=as.polygons(survey_grid_ncrmp.sr$strat)
   survey_grid_ncrmp.sh$str_name=tabLU[survey_grid_ncrmp.sh$strat]
   
+  #determine UTM 
   survey_grid_ncrmp.sf=st_set_crs(x = st_as_sf(survey_grid_ncrmp.sh),value=32655)
   
   st_write(obj = survey_grid_ncrmp.sf,
