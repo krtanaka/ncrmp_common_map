cb1eb792774c649b68718bab2f9e1bab0f717cd5
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-06-03 10:52:22 -1000

    Update McCoy_add_sector_to_sitevisit.R

--- scripts/misc/McCoy_add_sector_to_sitevisit.R
+++ scripts/misc/McCoy_add_sector_to_sitevisit.R
@@ -1,26 +1,50 @@
 ################################################
-### add sector information to site visit #######
+### Add sector information to site visit #######
 ################################################
-# MUST CONNECT TO THE T DRIVE AND X DRIVE
 
-rm(list=ls())
+rm(list = ls())
+
 library(readr)
-library(rgdal)    # needed for working with shapefiles
 library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(maps)
+library(sp)
+library(sf)
+
+# Ensure you are connected to the T Drive and X Drive
+
+get_utm <- function(x, y, zone, loc){
+  points = SpatialPoints(cbind(x, y), proj4string = CRS("+proj=longlat +datum=WGS84"))
+  points_utm = spTransform(points, CRS(paste0("+proj=utm +zone=", zone[1]," +ellps=WGS84 +north")))
+  if (loc == "x") {
+    return(coordinates(points_utm)[,1])
+  } else if (loc == "y") {
+    return(coordinates(points_utm)[,2])
+  }
+}
 
 # READ IN DATA ------------------------
-x<-read.csv("T:/DataManagement/NCEI Archive Packages/Fish REA nSPC (NCRMP)/Data/V0_FISH_REA_MARIAN_2022.csv")
+x <- read.csv("T:/DataManagement/NCEI Archive Packages/Fish REA nSPC (NCRMP)/Data/2021/V0_FISH_REA_OAHU 2021.csv")
 
 # ADD SECTOR INFO ---------------------
+survey <- x %>%
+  select(OBS_YEAR, REGION, REGION_NAME, ISLAND, SITE, DATE_, REEF_ZONE, DEPTH_BIN, LATITUDE, LONGITUDE, SITEVISITID, METHOD) %>%
+  distinct(SITEVISITID, .keep_all = TRUE)
 
-survey<-x %>% select(OBS_YEAR, REGION, REGION_NAME, ISLAND, SITE, DATE_, REEF_ZONE, DEPTH_BIN, LATITUDE, LONGITUDE, SITEVISITID, METHOD)
-survey<- distinct(survey, SITEVISITID, .keep_all = TRUE)
-df = survey
+df <- survey
 
-# re-project in correct utm zone 
-# For Guam, UTM 55 
 zone <- (floor((df$LONGITUDE[1] + 180)/6) %% 60) + 1
-xy_utm = as.data.frame(cbind(utm = project(as.matrix(df[, c("LONGITUDE", "LATITUDE")]), paste0("+proj=utm +units=m +zone=", zone))))
+
+xy_utm = data.frame(x = df$LONGITUDE, y = df$LATITUDE)  %>% 
+  mutate(zone2 = (floor((x + 180)/6) %% 60) + 1, keep = "all") %>% 
+  group_by(zone2) %>% 
+  mutate(utm_x = get_utm(x, y, zone2, loc = "x"),
+         utm_y = get_utm(x, y, zone2, loc = "y")) %>% 
+  ungroup() %>% 
+  select(utm_x, utm_y) %>% 
+  as.data.frame()
+
 colnames(xy_utm) = c("X", "Y")
 df = cbind(df, xy_utm)
 
@@ -29,11 +53,14 @@ latlon = df[,c("X", "Y")]
 coordinates(latlon) = ~X+Y
 
 # read shp file, assign same projection attribute
-shp <- rgdal::readOGR("X:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/sector/gua_base_land_openwater_mpa_finalize.shp")
-CRS.new <- CRS("+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs")
+shp <- st_read(file.path("X:/GIS/Projects/CommonMaps/01_Preprocess/MARI/GUA/sector/gua_base_land_openwater_mpa_finalize.shp")) %>% as("Spatial")
+
+if (median(df$LATITUDE) > 0) CRS.new <- CRS(paste0("+proj=utm +zone=", zone, " +datum=WGS84 +units=m +no_defs +north"))
+if (median(df$LATITUDE) < 0) CRS.new <- CRS(paste0("+proj=utm +zone=", zone, " +datum=WGS84 +units=m +no_defs +south"))
+
 proj4string(latlon) <- CRS.new
 proj4string(shp) <- CRS.new # WARNING MESSAGE OK
-area <- over(latlon,shp)
+area <- over(latlon, shp)
 
 # combine with site data
 df = cbind(df, as.data.frame(area))
