3043dad960ac5c75f4b12ae3f8969d3d2b019808
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-06-13 10:57:38 -1000

    ok

--- scripts/misc/make_single_strat_area_ID_table.R
+++ scripts/misc/make_single_strat_area_ID_table.R
@@ -46,7 +46,7 @@ for (isl in 1:length(islands)) {
   
 }
 
-df_all %>% 
+df_all = df_all %>% 
   mutate(across(where(is.character), toupper))
 
 write_csv(df_all, file = paste0("outputs/tables/strata_table_", region, ".csv"))
