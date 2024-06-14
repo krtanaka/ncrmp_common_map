445830b750410533ee4e757e443011525cc37ff0
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-06-14 12:53:51 -1000

    Update 5_generate_survey_sites.R

--- scripts/5_generate_survey_sites.R
+++ scripts/5_generate_survey_sites.R
@@ -80,7 +80,7 @@ ggmap::register_google("AIzaSyDpirvA5gB7bmbEbwB1Pk__6jiV4SXAEcY")
 
 for (i in 1:length(islands)) {
   
-  # i = 9
+  # i = 8
   
   # survey domain with sector & reef & hard_unknown & 3 depth bins
   load(paste0("data/survey_grid_ncrmp/survey_grid_", islands[i], ".RData"))#; plot(survey_grid_ncrmp)
@@ -233,25 +233,6 @@ for (i in 1:length(islands)) {
   }
   
   nearby_sites <- remove_close_points(nearby_sites, 100)#; points(nearby_sites, pch = 20, col = 4)
-  
-  ### geos got deprecated so this is getting commendted out
-  # coordinates(nearby_sites) <- c('longitude', 'latitude')
-  # proj4string(nearby_sites) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
-  # nearby_sites <- spTransform(nearby_sites, CRS(paste0("+proj=utm +units=m +zone=", utm_i$UTM_Zone, " ", utm_i$Hemisphere)))##
-  # 
-  # library(rgeos)
-  # points_matrix <- gWithinDistance(nearby_sites, dist = 100, byid = T)
-  # points_matrix[lower.tri(points_matrix, diag = T)] <- NA
-  # points_matrix
-  # 
-  # colSums(points_matrix, na.rm = T) == 0
-  # 
-  # v <- colSums(points_matrix, na.rm = T) == 0
-  # 
-  # nearby_sites <- spTransform(nearby_sites, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-  # nearby_sites = as.data.frame(nearby_sites)
-  # 
-  # nearby_sites = nearby_sites[v, ]#; points(nearby_sites, pch = 20, col = 4)
   
   colnames(nearby_sites) = c("longitude", "latitude")
   nearby_sites$latitude = round(nearby_sites$latitude, 4)
