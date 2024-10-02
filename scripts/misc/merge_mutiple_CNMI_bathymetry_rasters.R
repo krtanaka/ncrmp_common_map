d6b2ea13609130fdbcb70d71e851c1bafaec7301
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-10-02 11:15:56 -1000

    Update merge_mutiple_CNMI_bathymetry_rasters.R

--- scripts/misc/merge_mutiple_CNMI_bathymetry_rasters.R
+++ scripts/misc/merge_mutiple_CNMI_bathymetry_rasters.R
@@ -4,55 +4,69 @@ library(tools)
 
 rm(list = ls())
 
-# Hawaii: Continuously Updated Digital Elevation Model (CUDEM) - 1/9 Arc-Second Resolution Bathymetric-Topographic Tiles
-files <- list_files_with_exts("/Users/kisei.tanaka/Desktop/cnmi2019_islands_Job1075101/", "tif")
+list_folders <- list.files('/Users/kisei.tanaka/Desktop/', full.names = TRUE)
+list_folders <- list_folders[file.info(list_folders)$isdir]
 
-path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "51m_res/") # Local without VPNs
-path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "30m_res/") # Local without VPNs
-path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "9m_res/") # Local without VPNs
-path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "3m_res/") # Local without VPNs
-
-if (!dir.exists(path)) dir.create(path, recursive = T)
-
-for (f in 1:length(files)) {
+for (l in head(list_folders, -2)) {
   
-  # f = 5
-  topo <- rast(files[f])
+  # l = list_folders[1]
   
-  # fact = 10 gives you 30m,
-  # fact = 17 gives you 51m, 
-  # fact = 5 gives you 13m
-  # fact = 3 gives you 9m
+  folder_name <- basename(l)
   
-  topo = terra::aggregate(topo, fact = 3, core = 14)
+  files <- list_files_with_exts(l, "tif")
   
-  topo[topo >= 0] <- NA
-  topo[topo <= -30] <- NA
-  topo = project(topo, "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-  plot(topo)
-  writeRaster(topo, paste0(path, basename(files[f])), overwrite = TRUE)
+  # path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "51m_res/") # Local without VPNs
+  # path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "30m_res/") # Local without VPNs
+  # path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "9m_res/") # Local without VPNs
+  # path = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/", "3m_res/") # Local without VPNs
+  path <- paste0("N:/GIS/Projects/CommonMaps/Bathymetry/", folder_name, "/3m_res/")
+  
+  if (!dir.exists(path)) dir.create(path, recursive = T)
+  
+  for (f in 1:length(files)) {
+    
+    # f = 1
+    topo <- rast(files[f])
+    
+    # fact = 10 gives you 30m,
+    # fact = 17 gives you 51m, 
+    # fact = 5 gives you 13m
+    # fact = 3 gives you 9m
+    
+    topo = terra::aggregate(topo, fact = 3, core = 14)
+    
+    topo[topo >= 0] <- NA
+    topo[topo <= -30] <- NA
+    topo = project(topo, "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    plot(topo)
+    writeRaster(topo, paste0(path, basename(files[f])), overwrite = TRUE)
+    
+  }
+  
+  # files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/51m_res/", "tif")
+  # merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_51m_res.tif", overwrite = T)
+  # plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_51m_res.tif"))
+  # 
+  # files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/30m_res/", "tif")
+  # merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_30m_res.tif", overwrite = T)
+  # plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_30m_res.tif"))
+  # 
+  # files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/9m_res/", "tif")
+  # merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_9m_res.tif", overwrite = T)
+  # plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_9m_res.tif"))
+  
+  files <- list_files_with_exts(path, "tif")
+  merge_rasters(files[1:length(files)], output_raster = paste0("N:/GIS/Projects/CommonMaps/Bathymetry/", folder_name, ".tif"), overwrite = T)
+  plot(rast(paste0("N:/GIS/Projects/CommonMaps/Bathymetry/", folder_name, ".tif")))
+  
+  # # calculate rugosity
+  # df = rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_3m_res.tif")
+  # df = terra::aggregate(df, fun = "sd", fact = 2, core = 14) # fact = 10 gives you 30m, fact = 17 gives you 51m, fact = 5 gives you 13m
+  # df = project(df, "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  # plot(df)
+  # writeRaster(df, "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_6m_res_rugosity.tif", overwrite = TRUE)
+  
   
 }
 
-files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/51m_res/", "tif")
-merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_51m_res.tif", overwrite = T)
-plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_51m_res.tif"))
 
-files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/30m_res/", "tif")
-merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_30m_res.tif", overwrite = T)
-plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_30m_res.tif"))
-
-files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/9m_res/", "tif")
-merge_rasters(files[1:16], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_9m_res.tif", overwrite = T)
-plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_9m_res.tif"))
-
-files <- list_files_with_exts("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi2019_islands/3m_res/", "tif")
-merge_rasters(files[1:13], output_raster = "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_3m_res.tif", overwrite = T)
-plot(rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_3m_res.tif"))
-
-# calculate rugosity
-df = rast("N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_3m_res.tif")
-df = terra::aggregate(df, fun = "sd", fact = 2, core = 14) # fact = 10 gives you 30m, fact = 17 gives you 51m, fact = 5 gives you 13m
-df = project(df, "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-plot(df)
-writeRaster(df, "N:/GIS/Projects/CommonMaps/Bathymetry/cnmi_merged_6m_res_rugosity.tif", overwrite = TRUE)
