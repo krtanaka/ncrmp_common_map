1d6813f99410dfc2fe2a339181f48f86bf39a009
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-10-17 10:09:32 -1000

    ok

--- scripts/misc/merge_Tutuila_bathy_files.R
+++ scripts/misc/merge_Tutuila_bathy_files.R
@@ -4,40 +4,31 @@ library(readr)
 
 rm(list = ls())
 
-# patch finer res bathymetry files first
+file_paths <- c(
+  "N:/GIS/Projects/CommonMaps/Bathymetry/tut_dball.asc",
+  "N:/GIS/Projects/CommonMaps/Bathymetry/tut_dbmb.asc",
+  # "N:/GIS/Projects/CommonMaps/Bathymetry/Tutuila_5m.asc",
+  "N:/GIS/Projects/CommonMaps/Bathymetry/cudem_ninth_amsam/cudem_ninth_amsam_J1078896.tif",
+  "N:/GIS/Projects/CommonMaps/Bathymetry/2022_ngs_topobathy_dem_tutuila/2022_ngs_topobathy_dem_tutuila_J1078893_000_000.tif"
+  # "N:/GIS/Projects/CommonMaps/Bathymetry/2022_ngs_topobathy_dem_tutuila/2022_ngs_topobathy_dem_tutuila_J1078893_001_000.tif"
+)
 
-b1 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/tut_dball.asc"); plot(b1); res(b1)
-b2 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/tut_dbmb.asc"); plot(b2); res(b2)
-b3 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/Tutuila_5m.asc"); plot(b3); res(b3)
-b4 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/cudem_ninth_amsam/cudem_ninth_amsam_J1078896.tif"); plot(b4); res(b4)
-b5 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/2022_ngs_topobathy_dem_tutuila/2022_ngs_topobathy_dem_tutuila_J1078893_000_000.tif"); plot(b5); res(b5)
-b6 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/2022_ngs_topobathy_dem_tutuila/2022_ngs_topobathy_dem_tutuila_J1078893_001_000.tif"); plot(b6); res(b6)
+rasters <- lapply(file_paths, rast)
 
-base = b3
+rasters <- lapply(rasters, function(raster) {
+  crop(raster,  ext(530550, 532100, 8416200, 8417300))
+})
 
-b1 = resample(b1, base, method = "near"); plot(b1)
-b2 = resample(b2, base, method = "near"); plot(b2)
-b3 = resample(b3, base, method = "near"); plot(b3) 
-b4 = resample(b4, base, method = "near"); plot(b4) 
-b5 = resample(b5, base, method = "near"); plot(b5)
-b6 = resample(b6, base, method = "near"); plot(b6)
+lapply(rasters, function(r) { plot(r); res(r) })
 
-fine_topo <- mean(c(b1, b2, b3, b4, b5, b6), na.rm = TRUE)
+base <- rasters[[4]]
 
+rasters_resampled <- lapply(rasters, function(r) resample(r, base, method = "near"))
+
+fine_topo <- mean(do.call(c, rasters_resampled), na.rm = TRUE)
 fine_topo[fine_topo >= 0] <- NA
 fine_topo[fine_topo <= -30] <- NA
-
 plot(fine_topo)
-save(topo_i, file = 'data/gis_bathymetry/tut_merged.RData')
-
-# Assign the UTM Zone 2S CRS to fine_topo
-crs(fine_topo) <- "EPSG:32702"
-
-# Reproject to WGS84 (lat/lon)
-fine_topo_latlon <- project(fine_topo, "EPSG:4326")
-
-# Plot the reprojected raster
-plot(fine_topo_latlon)
 
 topo_i = readAll(raster(fine_topo))
 
