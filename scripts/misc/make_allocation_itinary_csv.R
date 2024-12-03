7d93f74a119a6c4e9a1376ac430c1d7dbbe0f9df
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-12-02 16:16:34 -1000

    ok

---
+++ scripts/misc/make_allocation_itinary_csv.R
@@ -0,0 +1,49 @@
+##################################################################
+### Exporting Strata ID, Areas as single table for each region ###
+##################################################################
+
+rm(list = ls())
+
+library(dplyr)
+library(ggplot2)
+library(raster)
+library(sp)
+library(tidyr)
+library(sf)
+library(readr)
+library(concaveman)
+library(patchwork)
+library(ggthemes)
+
+utm = read_csv('data/misc/ncrmp_utm_zones.csv')
+
+regions = c("S.MARIAN",
+            "N.MARIAN", 
+            # "SAMOA",
+            "PRIAs")
+            # "MHI", 
+            # "NWHI") 
+
+df_all = NULL
+
+for (r in 1:length(regions)) {
+  
+  # r = 7
+  
+  df = read_csv(paste0("outputs/tables/strata_table_", regions[r], ".csv")) %>%
+    mutate(Island_Code = tolower(island))
+  
+  df <- left_join(df, utm)
+  
+  df = df %>% 
+    dplyr::select(Island, Island_Code, sector_id, reef_id, depth_bin, area_km2)
+  
+  df_all = rbind(df_all, df)
+  
+}
+
+df_all = df_all %>% 
+  mutate(across(where(is.character), toupper)) %>%
+  rename_with(toupper)
+
+write_csv(df_all, file = "data/misc/draft_itinerary.csv")
