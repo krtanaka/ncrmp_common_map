09252f5a068602bcdb7e4e96f4bb83160b2812f0
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-12-05 16:50:54 -1000

    ok

--- scripts/misc/merge_Guam_bathy_files.R
+++ scripts/misc/merge_Guam_bathy_files.R
@@ -9,6 +9,7 @@ b1 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/gua_mb_ld.asc"); plot(b1); res(
 b2 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/gua_nthmp_dem_10m_mosaic.tif"); plot(b2); res(b2)
 b3 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/guam_5m.asc"); plot(b3); res(b3)
 b4 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/Guam_60m.asc"); plot(b4); res(b4)
+b5 = rast("N:/GIS/Projects/CommonMaps/Bathymetry/ngdc_bathy_10m_guam_049d_6725_8fc5.nc"); plot(b5); res(b5)
 
 utm = read_csv('data/misc/ncrmp_utm_zones.csv')
 island_boxes = read_csv("data/misc/Island_Extents.csv") # Updated Bounding boxes 2021
@@ -20,8 +21,9 @@ utm_i = utm %>% subset(Island_Code == "gua")
 if (mean(yFromRow(b1, 1:nrow(b1))) > 0) sr = paste0('+proj=utm +zone=', utm_i$UTM_Zone, ' +datum=WGS84 +units=m +no_defs +north')
 if (mean(yFromRow(b1, 1:nrow(b1))) < 0) sr = paste0('+proj=utm +zone=', utm_i$UTM_Zone, ' +datum=WGS84 +units=m +no_defs +south')
 
-b1 <- project(b1, sr); plot(b1)
-b2 <- project(b2, sr); plot(b2)
+crs(b5) <- "+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs"
+
+b5 <- project(b5, sr); plot(b5)
 
 res = b3
 
@@ -29,8 +31,9 @@ b1 = resample(b1, res, method = "near"); plot(b1)
 b2 = resample(b2, res, method = "near"); plot(b2)
 b3 = resample(b3, res, method = "near"); plot(b3)
 b4 = resample(b4, res, method = "near"); plot(b4)
+b5 = resample(b5, res, method = "near"); plot(b5)
 
-fine_topo = mean(c(b1, b2, b3, b4), na.rm = T)
+fine_topo = mean(c(b1, b2, b3, b4, b5), na.rm = T)
 
 fine_topo[fine_topo >= 0] <- NA
 fine_topo[fine_topo <= -30] <- NA
